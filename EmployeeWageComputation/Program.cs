b7bf760e061ab5b2858918f681ba9bc4a95c698c
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-17 22:13:20 +0530

    [sakshi] modifying program.cs to check the attendance of employee

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to employee wage computation ");
+            const int IS_PRESENT = 1;
+            Console.WriteLine("welcome to EmployeeWageComputation");
+            Random random = new Random();
+            int emplyeeInput = random.Next(0, 2);// 0  or 1
+            if (emplyeeInput == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
