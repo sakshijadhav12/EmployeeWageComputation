e1068bd7b0425813f753e7d69d33cabd80423970
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-18 00:34:28 +0530

    [sakshi] modifying program.cs to implement classes and methods

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,49 +1,58 @@
-﻿namespace EmployeeWageComputation
-{
-   internal  class Program
+﻿
+ namespace EmployeeWageComputation
     {
+        class Program
+        {
 
-        static void Main(string[] args)
-        {   //constants
+            //constants
             const int IS_PART_TIME = 1;
             const int IS_FULL_TIME = 2;
             const int EMP_RATE_PER_HOUR = 20;
             const int NUM_OF_WORKING_DAYS = 20;
             const int MAX_WORKING_HRS = 100;
-            //variables
-            int empHrs = 0, empWage = 0, totalWage = 0, day = 1, totalWorkingHrs = 0;
-            Console.WriteLine("welcome to EmployeeWageComputation");
-            Random random = new Random();
-            while (day <= NUM_OF_WORKING_DAYS && totalWorkingHrs <= MAX_WORKING_HRS)
+            public
+                 static int computeEmpWage()
             {
-                int emplyeeInput = random.Next(0, 3);
-                switch (emplyeeInput)
+                //variables
+                int empHrs = 0, empWage = 0, totalWage = 0, day = 1, totalWorkingHrs = 0;
+                Console.WriteLine("welcome to EmployeeWageComputation");
+                Random random = new Random();
+                while (day <= NUM_OF_WORKING_DAYS && totalWorkingHrs <= MAX_WORKING_HRS)
                 {
-                    case IS_PART_TIME:
-                        Console.WriteLine("part time employee is present ");
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        Console.WriteLine("full time employee is present ");
-                        empHrs = 8;
-                        break;
-                    default:
-                        Console.WriteLine(" employee is absent ");
-                        empHrs = 0;
-                        break;
-                }
+                    int emplyeeInput = random.Next(0, 3);
+                    switch (emplyeeInput)
+                    {
+                        case IS_PART_TIME:
+                            Console.WriteLine("part time employee is present ");
+                            empHrs = 4;
+                            break;
+                        case IS_FULL_TIME:
+                            Console.WriteLine("full time employee is present ");
+                            empHrs = 8;
+                            break;
+                        default:
+                            Console.WriteLine(" employee is absent ");
+                            empHrs = 0;
+                            break;
+                    }
 
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalWage += empWage;
-                Console.WriteLine("Day{0} Employeewage is :{1}", day, empWage);
-                day++;
-                totalWorkingHrs += empHrs;
-                if (totalWorkingHrs > MAX_WORKING_HRS)
-                {
-                    totalWorkingHrs = MAX_WORKING_HRS - empHrs;
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    totalWage += empWage;
+                    Console.WriteLine("Day{0} Employeewage is :{1}", day, empWage);
+                    day++;
+                    totalWorkingHrs += empHrs;
+                    if (totalWorkingHrs > MAX_WORKING_HRS)
+                    {
+                        totalWorkingHrs = MAX_WORKING_HRS - empHrs;
+                    }
                 }
+                Console.WriteLine(" Total Employee Wage  for {0} days and Hrs:{1} is :{2}", (day - 1), (totalWorkingHrs), (totalWage));
+                return totalWage;
             }
-            Console.WriteLine(" Total Employee Wage  for {0} days and Hrs:{1} is :{2}", (day - 1), (totalWorkingHrs), (totalWage));
+            static void Main(string[] args)
+            {
+                computeEmpWage();
+            }
+
         }
     }
-}
