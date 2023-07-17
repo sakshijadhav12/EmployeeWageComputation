45dc3cc757e2c696bb0a6171c521036d60379d64
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-17 22:09:42 +0530

    [sakshi] creating employeewage computation program

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee wage computation ");
+        }
+    }
+}
