2b350e8812e311e1fb9f96a9f00ad1fbe2ddb12e
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-17 22:22:59 +0530

    [sakshi] modifying program .cs to add part time employee

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -3,17 +3,25 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            const int IS_PRESENT = 1;
-            const int EMP_RATE_PER_HR = 20;
+        {   //constants
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
+            //variables
             int empHrs = 0;
             int empWage = 0;
             Console.WriteLine("welcome to EmployeeWageComputation");
             Random random = new Random();
-            int emplyeeInput = random.Next(0, 2);// 0  or 1
-            if (emplyeeInput == IS_PRESENT)
+            //Computation
+            int emplyeeInput = random.Next(0, 3);
+            if (emplyeeInput == IS_PART_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is part time");
+                empHrs = 4;
+            }
+            else if (emplyeeInput == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is full time");
                 empHrs = 8;
             }
             else
@@ -21,7 +29,7 @@
                 Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HR;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Daily Employeewage is :{0}", empWage);
         }
     }
